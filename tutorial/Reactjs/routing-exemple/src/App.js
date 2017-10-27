ff92859c5f1b06610f6083d4da4460739808b48b
Koumaran <jsivanes42@gmail.com>
2017-10-27 09:52:41 +0200

    react tuto

---
+++ tutorial/Reactjs/routing-exemple/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+const Home = () => <h1>Home</h1>;
+
+const About = () => <h1>About</h1>;
+
+const App = () => (
+    <Router>
+    <div>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+    </div>
+    </Router>
+);
+
+export default App;
