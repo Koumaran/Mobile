17d4e3843eafa401976d047944154192679c6d1b
Koumaran <jsivanes42@gmail.com>
2017-11-24 16:07:28 +0100

    Bull's Eye done

---
+++ Swift/BullsEye/BullsEye/AboutMeViewController.swift
@@ -0,0 +1,28 @@
+//
+//  AboutMeViewController.swift
+//  BullsEye
+//
+//  Created by Sivanesan on 24/11/2017.
+//  Copyright © 2017 Sivanesan jegathas. All rights reserved.
+//
+
+import UIKit
+
+class AboutMeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func closeTapped() {
+        dismiss(animated: true, completion: nil)
+    }
+
+}
